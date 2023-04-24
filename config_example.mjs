cf2ba2bbc5826cada78d2eb10fbf322eb009010d
dennisstinauer <dennisstinauer@gmail.com>
2023-04-24 20:11:44 +0200

    added example config.

---
+++ config_example.mjs
@@ -0,0 +1,15 @@
+//########################################################################
+//####                       EXAMPLE CONFIG                           ####
+//####                                                                ####
+//####       Replace all the X*es with your respective token.         ####
+//####                                                                ####
+//########################################################################
+
+const config = {
+  token:
+    "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  clientId: "XXXXXXXXXXXXXXXXXX",
+  mongoDbToken: "mongodb+srv://XXX.mongodb.net/?retryWrites=true&w=majority",
+};
+
+export default config;
